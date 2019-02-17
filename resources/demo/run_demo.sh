46b114081aae8f06e03c7732d1bb5ef3425748d2
Nathanaël MOREL <nathanael.morel@altran.com>
2019-02-17 22:16:06 +0100

    Preparation demo

---
+++ resources/demo/run_demo.sh
@@ -0,0 +1,15 @@
+cd ../sql
+./run_sql_demo.sh
+cd ../../
+echo "Packaging ...\n"
+mvn package
+echo "Deploiement ...\n"
+sudo mv webservice/target/Library\ webservice.war $CATALINA_HOME/webapps/Library_webservice.war
+sudo mv webapp/target/Library\ webapp.war $CATALINA_HOME2/webapps/Library_webapp.war
+cd $CATALINA_HOME/bin
+echo "Running webservice"
+sudo ./startup.sh
+cd $CATALINA_HOME2/bin
+echo "Running webapp"
+sudo ./startup.sh
+cd ~/OpenClassrooms/project03_library/
