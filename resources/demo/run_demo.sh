6ba5d34e45fd35a7336b76b0a6429288c4e89fbf
Nathanaël MOREL <nathanael.morel@altran.com>
2019-02-24 19:49:18 +0100

    Mise à jour fichier run_demo.sh

--- resources/demo/run_demo.sh
+++ resources/demo/run_demo.sh
@@ -1,9 +1,9 @@
 cd ../sql
 ./run_sql_demo.sh
 cd ../../
-echo "Packaging ...\n"
+echo "Packaging ..."
 mvn package
-echo "Deploiement ...\n"
+echo "Deploiement ..."
 sudo mv webservice/target/Library\ webservice.war $CATALINA_HOME/webapps/Library_webservice.war
 sudo mv webapp/target/Library\ webapp.war $CATALINA_HOME2/webapps/Library_webapp.war
 cd $CATALINA_HOME/bin
@@ -12,4 +12,3 @@ sudo ./startup.sh
 cd $CATALINA_HOME2/bin
 echo "Running webapp"
 sudo ./startup.sh
-cd ~/OpenClassrooms/project03_library/
